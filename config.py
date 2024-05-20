79cb9246e60a31ea2ac205a48e38d0924466af4f
anton <antonkuzmn1@gmail.com>
2024-05-21 01:51:42 +0700

    fix cors

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ BACKEND_APP = "src.rest_controller:app"
 BACKEND_HOST = "127.0.0.1"
 BACKEND_PORT = 8080
 BACKEND_RELOAD = True
+
+ALLOWED_ORIGINS = ['*']
+# ALLOWED_ORIGINS = [
+#     "https://localhost",
+#     "http://localhost:4200",
+#     "http://localhost:8080",
+# ]
--- src/rest_controller.py
+++ src/rest_controller.py
@@ -2,13 +2,23 @@
 import uvicorn
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from starlette.middleware.cors import CORSMiddleware
 
-from config import BACKEND_APP, BACKEND_PORT, BACKEND_RELOAD, BACKEND_HOST
+from config import BACKEND_APP, BACKEND_PORT, BACKEND_RELOAD, BACKEND_HOST, ALLOWED_ORIGINS
 from src.database import get_db
 from src.models import Item
 from src.parse_data import get_data
 
 app = FastAPI()
+
+# noinspection PyTypeChecker
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/get_players/")
